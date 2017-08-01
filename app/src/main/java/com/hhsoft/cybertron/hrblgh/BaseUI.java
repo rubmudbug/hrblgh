504048b0d469f879fac6b49b209e9b89e092f4a7
11929 <1192958015@qq.com>
2017-08-01 09:10:16 +0800

    Initial commit

---
+++ app/src/main/java/com/hhsoft/cybertron/hrblgh/BaseUI.java
@@ -0,0 +1,19 @@
+package com.hhsoft.cybertron.hrblgh;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+    }
+}
